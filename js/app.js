229fb100124db4567b11ee87e6e3fbbba26ff6a5
Daniel Diaz Suarez <ddiaz@autentia.com>
2013-04-10 16:21:36 +0200

    Ya selecciona color

--- js/app.js
+++ js/app.js
@@ -9,35 +9,6 @@ App.Router.map(function() {
 	})
 });
 
-App.MenuSelectColorRoute = Ember.Route.extend({
-	setupController : function(){
-		
-	}/*,
-	model : function(){
-		App.colors=[];
-		var color1 = App.Color.create({name: 'red', isChecked: false})
-		var color2 = App.Color.create({name: 'green', isChecked: false})
-		var color3 =App.Color.create({name: 'blue', isChecked: false})
-			App.colors.push(color1)
-			App.colors.push(color2)
-			App.colors.push(color3)
-		return App.colors;
-	}*/
-});
-
-App.uncheck = function(resto, actual){
-	for(var i = 0; i < resto.length; i++){
-		if(resto[i].get("name") != actual.get("name"))
-			resto[i].set("isChecked", false);
-	}
-}
-
-App.MenuWatchColorRoute = Ember.Route.extend({
-	setupController : function(){
-		
-	}
-});
-
 App.User = Ember.Object.extend({
 	name : "",
 	nameRepeated : "",
@@ -60,35 +31,45 @@ App.User = Ember.Object.extend({
 		return empty
 	}.property("name","nameRepeated")
 });
+
 /* Creo un objeto Color */
-App.Color = Ember.Object.extend({
-	name: "",
-	isChecked: false,
-	changed: function(){
-		console.log(this.get('isChecked'))
-		App.uncheck(App.colorController.colores, this);
-	}.observes("isChecked")
-});
+
 App.IndexRoute = Ember.Route.extend({
 
 });
+
 App.mainUser = App.User.create({
 
 });
-/*
-//Creacion correcta del array, pero no la manera adecuada
-App.colores = Ember.makeArray([
-	amarillo = App.Color.create({name: "yellow"}),
-	rojo = App.Color.create({name: "red"}),
-	blue = App.Color.create({name: "blue"})
-]);
-*/
-//Vamos a crear un controlador de colores
 
-App.colorController = Ember.ArrayController.create({
-	colores: [
-		App.Color.create({name: 'red', isChecked: false}),
-		App.Color.create({name: 'green', isChecked: false}),
-		App.Color.create({name: 'blue', isChecked: false})
-	]
+App.Color = Ember.Object.extend({
+	name: ""
+});
+
+App.selectedColorName = "red";
+
+App.MenuSelectColorController = Ember.ArrayController.extend({
+	changeSelectedColor : function(selectedColor){
+		alert("cambiando de "+ App.selectedColorName + " a " +selectedColor.get("name"))
+		App.set("selectedColorName", selectedColor.get("name"));
+	}, 
+})
+
+App.MenuSelectColorRoute = Ember.Route.extend({
+	model : function(){	
+		App.colors=[];
+		var color1 = App.Color.create({name: 'red', isChecked: false})
+		var color2 = App.Color.create({name: 'yellow', isChecked: false})
+		var color3 =App.Color.create({name: 'blue', isChecked: false})
+			App.colors.push(color1)
+			App.colors.push(color2)
+			App.colors.push(color3)
+		return App.colors;
+	}
+});
+
+App.MenuWatchColorRoute = Ember.Route.extend({
+	setupController : function(){
+		
+	}
 });
